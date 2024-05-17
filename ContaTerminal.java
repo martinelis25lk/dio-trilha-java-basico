4c5dea3cc177b93217a23ba612d6ac201aa0c220
martinelis25lk <gui.almeida.edu@gmail.com>
2024-05-16 21:32:27 -0300

    commit de programa simples simulando uma conta bancaria

--- ContaTerminal.java
+++
@@ -1,9 +0,0 @@
-public class ContaTerminal {
-
-    public static void main(String[] args) {
-        System.out.println("hello world");
-
-        System.out.println("teste");
-        System.out.println("aaa");
-    }
-}
